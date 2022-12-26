3148fa8a7256513f21d25a891d495e3e4392b8c6
balaji-githubstore <balaji0017@gmail.com>
2022-12-26 16:21:22 +0530

    Static and Non-Static Variable

---
+++ EmployeeProject/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProject
+{
+    public class Employee
+    {
+        public int empId;  //non-static variable or instance variable 
+        public string empName;
+        public double empSalary;
+        public static string companyName; //static variable or class variable 
+        public static string companyLocation;
+
+
+    }
+}
---
+++ EmployeeProject/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using EmployeeProject;
+
+namespace Maveric.Runner
+{
+    public class Program
+    {
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("************EMPLOYEE DASHBORD*****************");
+
+            Employee.companyName = "Maveric";
+            Employee.companyLocation = "Chennai";
+
+
+            Employee emp1=new Employee();
+            Employee emp2=new Employee();
+
+
+            emp1.empId = 101;
+            emp1.empName = "Saul";
+            emp1.empSalary = 9000;
+
+            emp2.empId = 102;
+            emp2.empName = "Kim";
+            emp2.empSalary = 12000.2;
+
+            
+
+
+            Employee.companyName = "Maveric Company";
+
+            Console.WriteLine(emp1.empId);
+
+            Console.WriteLine(emp2.empId);
+       
+        }
+
+    }
+}
