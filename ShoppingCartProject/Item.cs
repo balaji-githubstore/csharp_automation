09f29ace7915aa47194321c523646740dad0affa
balaji-githubstore <balaji0017@gmail.com>
2022-12-27 17:11:00 +0530

    Shopping Cart - Properties, Constructor, Constructor Overloading

---
+++ ShoppingCartProject/Item.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartProject
+{
+    public class Item
+    {
+        public int id;
+        private string description;
+        private int _quantity;
+        public double price;
+
+        //public Item()
+        //{
+        //    _quantity = 1;
+        //    Console.WriteLine("Object created");
+        //}
+
+        public Item(int _quantity)
+        {
+            this._quantity = _quantity;
+        }
+
+        //public Item(int _quantity, string description)
+        //{
+        //    this._quantity = _quantity;
+        //    this.description = description;
+        //}
+
+        //public Item(string description, int _quantity)
+        //{
+        //    this._quantity = _quantity;
+        //    this.description = description;
+        //}
+        //public Item(double price)
+        //{
+
+        //}
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+
+        public int Quantity
+        {
+            set
+            {
+                if (value > 0)
+                {
+
+                    _quantity = value;
+                }
+                else
+                {
+                    Console.WriteLine("Qty cannot be negative!!");
+                }
+            }
+        }
+
+        public void DisplayItemDetail()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine(id);
+            Console.WriteLine(description);
+            Console.WriteLine(_quantity);
+            Console.WriteLine(price);
+            Console.WriteLine("-------------------");
+        }
+
+
+        public static Item CreateInstance(int quantity)
+        {
+            Item i = new Item(quantity);
+
+            return i;
+        }
+
+        public static void PrintDescription(Item i)
+        {
+            Console.WriteLine(i.Description);
+        }
+
+
+
+        public void PrintDiscountedPrice()
+        {
+            //int price = 0;
+            if (_quantity == 2)
+            {
+                Console.WriteLine((this.price - (price * 10 / 100.0)) * _quantity);
+            }
+            else if (_quantity >= 3 && _quantity <= 5)
+            {
+                Console.WriteLine((price - (price * 15 / 100.0)) * _quantity);
+            }
+            else if (_quantity > 5)
+            {
+                Console.WriteLine((price - (price * 25 / 100.0)) * _quantity);
+            }
+            else
+            {
+                Console.WriteLine("No discount");
+            }
+            Console.WriteLine("-------------------");
+        }
+    }
+}
---
+++ ShoppingCartProject/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ShoppingCartProject
+{
+    public class ShoppingCartRunner
+    {
+
+        static void Main(string[] args)
+        {
+            Item i1 = new Item(5);
+            Item i2=new Item(5);
+
+            Item i3=Item.CreateInstance(10);
+            Item i4 = Item.CreateInstance(700);
+
+            i1.id = 101;
+            //i1.description = "Laptop";
+            i1.Description= "Laptop";
+            //i1.Quantity = 8;
+            i1.price = 1000;
+
+            i2.id = 102;
+            i2.Description = "Watch";
+            //i2.Quantity = -3;
+            i2.price = 5000;
+
+            Console.WriteLine(i1.Description);
+
+            //Console.WriteLine(i1.GetHashCode());
+            //Console.WriteLine(i2.GetHashCode());
+
+            i1.DisplayItemDetail();
+            i1.PrintDiscountedPrice();
+
+            i2.DisplayItemDetail();
+            i2.PrintDiscountedPrice();
+
+
+            i3.DisplayItemDetail();
+            i3.PrintDiscountedPrice();
+
+            i4.DisplayItemDetail();
+            i4.PrintDiscountedPrice();
+
+
+            Item.PrintDescription(i2);
+
+            Item.PrintDescription(i1);
+
+
+            Item.PrintDescription(i3);
+
+            Item.PrintDescription(i4); //pass by reference
+
+
+            Item i5=Item.CreateInstance(800);
+            i5.DisplayItemDetail();
+
+            Item.CreateInstance(900).DisplayItemDetail();
+        }
+    }
+}
