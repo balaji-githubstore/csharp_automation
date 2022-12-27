c7169d1967e176842e2297738602d0bc84197c7a
balaji-githubstore <balaji0017@gmail.com>
2022-12-27 18:10:47 +0530

    Selenium Basics

---
+++ SeleniumConcept/Demo1Basics.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chromium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumConcept
+{
+    public class Demo1Basics
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://github.com/login"; //set //navigate to the url
+
+            string title = driver.Title;  //get the current title
+            Console.WriteLine(title);
+
+            Console.WriteLine(driver.Url); //get  the current url
+            //get the page source and print it
+            Console.WriteLine(driver.PageSource);
+        }
+    }
+}
---
+++ SeleniumConcept/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Chromium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumConcept
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://github.com/login";
+
+            //By usernameLocator = By.Id("login_field");
+            //IWebElement element = driver.FindElement(usernameLocator);
+            //element.SendKeys("hello@gmail.com");
+
+
+            driver.FindElement(By.Id("login_field")).SendKeys("jacl@gmail.com");
+            driver.FindElement(By.Id("password")).SendKeys("hello");
+
+            driver.FindElement(By.Name("commit")).Click();
+        }
+    }
+}
