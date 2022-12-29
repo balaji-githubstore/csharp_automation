815e73540e483a1f34edd94f11cfd52073deb599
balaji-githubstore <balaji0017@gmail.com>
2022-12-29 17:51:02 +0530

    Multiple tabs Concept

---
+++ SeleniumAdvance/MultipleTabsCitiTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAdvance
+{
+    public class MultipleTabsCitiTest
+    {
+        [Test]
+        public void ApplyCreditTest()
+        {
+//            1.Navigate onto https://www.online.citibank.co.in/
+//2.Close if any pop up comes
+//3.Click on Login
+//4.Click on Forgot User ID?
+//5.Choose Credit Card
+//6.Enter credit card number as 4545 5656 8887 9998
+//7.Enter cvv number
+//8.Enter date as “14 / 04 / 2022”
+//9.Click on Proceed
+//10.Get the text and print it “Please accept Terms and Conditions”
+        }
+    }
+}
---
+++ SeleniumAdvance/MultipleTabsTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maveric.SeleniumAdvance
+{
+    public class MultipleTabsTest
+    {
+        [Test]
+        public void Demo1PhpMyAdminTabTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://www.db4free.net/";
+
+            //click on phpMyAdmin
+            driver.FindElement(By.PartialLinkText("phpMyAdmin")).Click();
+
+            ReadOnlyCollection<string> windows= driver.WindowHandles;
+
+            Console.WriteLine(windows[0]);
+            Console.WriteLine(windows[1]);
+            Console.WriteLine(driver.WindowHandles[1]);
+        }
+
+
+        [Test]
+        public void Demo2PhpMyAdminTabTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://www.db4free.net/";
+
+            //click on phpMyAdmin
+            driver.FindElement(By.PartialLinkText("phpMyAdmin")).Click();
+
+            
+            //switch to second tab
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+            driver.FindElement(By.Id("input_username")).SendKeys("john");
+            //enter password as admin
+            //click on login
+
+            driver.Close(); //check what is happening 
+
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+
+            Console.WriteLine(driver.Title);
+
+            driver.Quit();
+        }
+    }
+}
