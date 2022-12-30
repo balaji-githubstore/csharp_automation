16ed15673ec497e2e48c51ec36d84601d5bd147e
balaji-githubstore <balaji0017@gmail.com>
2022-12-30 13:12:09 +0530

    Selenium - frame, alert, javascript

---
+++ SeleniumAdvance/FrameTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maveric.SeleniumAdvance
+{
+    public class FrameTest
+    {
+        [Test]
+        public void Demo1Frame()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://netbanking.hdfcbank.com/netbanking/";
+
+            driver.SwitchTo().Frame(driver.FindElement(By.XPath("//frame[@name='login_page']")));
+
+            //enter userid as kevin123
+            driver.FindElement(By.Name("fldLoginUserId")).SendKeys("kevin123");
+            //click on continue
+            driver.FindElement(By.LinkText("CONTINUE")).Click();
+
+
+            //Switch back to main html
+            driver.SwitchTo().DefaultContent();
+
+
+        }
+
+        [Test]
+        public void Demo2Alert()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://netbanking.hdfcbank.com/netbanking/IpinResetUsingOTP.htm";
+
+            //click on go 
+            driver.FindElement(By.XPath("//img[@alt='Go']")).Click();
+
+
+            //wait for alert present - 
+
+            string alertText=driver.SwitchTo().Alert().Text;
+            Console.WriteLine(alertText);
+
+            driver.SwitchTo().Alert().Accept();
+        }
+
+    }   
+}
--- SeleniumAdvance/MultipleTabsCitiTest.cs
+++ SeleniumAdvance/MultipleTabsCitiTest.cs
@@ -1,28 +1,75 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Extensions;
 
-namespace SeleniumAdvance
+namespace Maveric.SeleniumAdvance
 {
     public class MultipleTabsCitiTest
     {
         [Test]
         public void ApplyCreditTest()
         {
-//            1.Navigate onto https://www.online.citibank.co.in/
-//2.Close if any pop up comes
-//3.Click on Login
-//4.Click on Forgot User ID?
-//5.Choose Credit Card
-//6.Enter credit card number as 4545 5656 8887 9998
-//7.Enter cvv number
-//8.Enter date as “14 / 04 / 2022”
-//9.Click on Proceed
-//10.Get the text and print it “Please accept Terms and Conditions”
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            driver.Url = "https://www.online.citibank.co.in/";
+
+            driver.FindElement(By.XPath("//a[@title='Close']")).Click();
+
+            driver.FindElement(By.XPath("//span[normalize-space()='Login']")).Click();
+
+            //switch to 2nd tab
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+
+
+            driver.FindElement(By.XPath("//div[contains(text(),'Forgot User')]")).Click();
+
+            driver.FindElement(By.LinkText("select your product type")).Click();
+
+            driver.FindElement(By.LinkText("Credit Card")).Click();
+
+            driver.FindElement(By.Id("citiCard1")).SendKeys("2344");
+
+            driver.FindElement(By.CssSelector("#citiCard2")).SendKeys("9898");
+
+            driver.FindElement(By.CssSelector("input[name='citiCard3']")).SendKeys("7872");
+
+            driver.FindElement(By.CssSelector("[name='citiCard4']")).SendKeys("7873");
+
+            driver.FindElement(By.Id("cvvnumber")).SendKeys("787");
+
+            //date field - approach 1 //not working in this page 
+            //driver.FindElement(By.Id("bill-date-long")).SendKeys("14/04/2022");
+
+
+            //date field - approac 2
+            //click and try to automate calendar
+
+            //driver.FindElement(By.Id("bill-date-long")).Click();
+            //SelectElement selectMonth = new SelectElement(driver.FindElement(By.CssSelector("select[data-handler='selectMonth']")));
+            //selectMonth.SelectByText("Apr");
+            //SelectElement selectYear = new SelectElement(driver.FindElement(By.CssSelector("select[data-handler='selectYear']")));
+            //selectYear.SelectByText("2000");
+            //driver.FindElement(By.LinkText("14")).Click();
+
+
+            //date field - approac 2 - use javascript to enter
+            driver.ExecuteJavaScript("document.querySelector('#bill-date-long').value='14/04/2000'");
+            driver.FindElement(By.CssSelector("[value='PROCEED']")).Click();
+
+            string actualError=driver.FindElement(By.XPath("//li[contains(text(),'accept Terms')]")).Text;
+            Console.WriteLine(actualError);
+
+            driver.Quit();
         }
     }
 }
